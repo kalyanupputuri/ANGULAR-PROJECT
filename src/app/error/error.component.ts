dc5de664d1aacb4228a267d0b1d7f6566a6f3c0d
Student <chandrahasmakarla@gmail.com>
2022-09-02 09:48:02 +0530

    first commit

---
+++ src/app/error/error.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-error',
+  templateUrl: './error.component.html',
+  styleUrls: ['./error.component.css']
+})
+export class ErrorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  errorMessage = 'An Error Occured! Please Contact The Support Team At Social Prachar' 
+}
